65d12134dee0149134768c61d6e9b3ad8e9c091a
Ahmed Alam <ahmedalam@Ahmeds-MacBook-Pro.local>
2022-11-28 09:51:15 +0500

    Binary To Ineger Functionality

--- addSubOfBinaryNumbers.cpp
+++ addSubOfBinaryNumbers.cpp
@@ -6,15 +6,35 @@ using std::string;
 void menu(int &x, int &y);
 void add();
 void sub();
+void userInput(int &x, string msg);
+void binToInt(int &x);
+void intToBin(int &x);
 
 int main()
 {
 
-    int x, y;
-    menu(x, y);
-
-
+    // int x, y;
+    // menu(x, y);
 }
+
+void binToInt(int &x)
+{
+    int i = x, count = 1;
+    x = 0;
+    for(; i > 0; i /= 10)
+    {
+        int rem = i % 10;
+        if(rem == 1)
+            x += count;
+        count *= 2;
+    }
+}
+
+void intToBin(int &x)
+{
+    
+}
+
 
 void userInput(int &x, string msg)
 {
@@ -32,4 +52,10 @@ void menu(int &x, int &y)
 {
     userInput(x, "First");
     userInput(y, "Second");
+    cout << "What do you want to perform: ";
+    cout << "1) Addition:\n";
+    cout << "2) Subtraction:\n";
+    binToInt(x);
+    binToInt(y);
+    cout << "Do you want to change the input Numbers";
 }
